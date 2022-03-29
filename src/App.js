0f143eac9790a92bc1a914acd61a04b0614dd7d3
mbnaveen <navinsmb@gmail.com>
2022-03-30 00:26:33 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './components/navbar';
 import {Router} from  './Routes/Router'
+import {Footer} from './components/footer';
 
 function App() {
   return (
     <div className="App">
      <Navbar/> 
 <Router/>
+<Footer/> 
     </div>
   );
 }
